9e4a8b368c1761e1d4613acfd73a030b3db7b6d9
Anmol Jagetia <anmoljagetia@gmail.com>
2016-01-01 19:36:56 +0530

    Adds Setup file for PyPI

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+ 
+setup(
+    name = 'BSSLint',
+    packages = ['BSSL'],
+    version = '0.1',
+    description = 'Derived from the popular CPPLint from Google, but modified to incorporate code styles by Dr. B.S. Sanjeev for my undergraduate Data Structures and Algorithms courses',
+    author='Anmol Jagetia',
+    author_email='anmoljagetia@gmail.com',
+    url='https://github.com/anmoljagetia/BSSlint'
+)
